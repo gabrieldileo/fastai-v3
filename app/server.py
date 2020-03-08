ee6e560a3f76122347c21e1c96ea7479a9d2e08e
3l_g@uch0 <30272732+gabrieldileo@users.noreply.github.com>
2020-03-08 11:12:06 +0100

    Update server.py
    changed url for branch to get EURUSD OHLC model

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1aeDBq55gEhuurEPr-wCCM5skEkzOKxbh'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1JGOzFfqXRhs_qVAJ_cllOD6ff5G2TdJH'
 #export_file_url = 'https://drive.google.com/drive/folders/1HQU4erwrghl0oGb-luFgZdP6ihDULCuT'
 export_file_name = 'export.pkl'
 
