089aecf62922d850c55e0d552bdbe9371b42f373
3l_g@uch0 <30272732+gabrieldileo@users.noreply.github.com>
2020-03-08 07:16:09 +0100

    Update server.py
    a new url

--- app/server.py
+++ app/server.py
@@ -10,7 +10,8 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/drive/folders/1HQU4erwrghl0oGb-luFgZdP6ihDULCuT'
+export_file_url = 'https://www.dropbox.com/l/scl/AAC5k82SDGSw6CqxxUg018nU0cZJBJB0ibE'
+#export_file_url = 'https://drive.google.com/drive/folders/1HQU4erwrghl0oGb-luFgZdP6ihDULCuT'
 export_file_name = 'export.pkl'
 
 classes = ['ham','salami']
